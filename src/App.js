d5520453764b30362dafeb16e064b1a731a2b766
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-13 15:17:23 +0100

    use map function to display a list of items in jsx

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
 import './App.css';
 
-const title = 'React';
-const welcome = {
-  greeting: 'Hey',
-  title: 'React'
-}
-function getTitle(title) {
-  return title;
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
 function App() {
   return (
     <div>
-      {/* <h1>Hello { title }</h1> */}
-      {/* {welcome.greeting} {welcome.title} */}
-      <h1>Hello {getTitle('React')}</h1>
+      <h1>My Hacker Stories</h1>
 
       <label htmlFor="search">Search: </label>
       <input type="text" id="search" />
+
+      <hr />
+
+      <ul>
+        {list.map(item => (
+          <li key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
