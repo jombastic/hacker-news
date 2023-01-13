f447277f0305a4e521842f37b33fb74b5dd4a02d
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-13 15:29:18 +0100

    create Search and List components and move logic from App to the new components

--- src/App.js
+++ src/App.js
@@ -24,23 +24,37 @@ function App() {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {list.map(item => (
-          <li key={item.objectID}>
-            <span>
-              <a href={item.url}>{item.title}</a>
-            </span>
-            <span>{item.author}</span>
-            <span>{item.num_comments}</span>
-            <span>{item.points}</span>
-          </li>
-        ))}
-      </ul>
+      <List />
+    </div>
+  );
+}
+
+const List = () => {
+  return (
+    <ul>
+      {list.map(item => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+const Search = () => {
+  return (
+    <div>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
     </div>
   );
 }
