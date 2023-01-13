b4c30efc33fb94e8b69fa72617666fe399b700ab
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-13 14:48:54 +0100

    utilize jsx in App component

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 const title = 'React';
+const welcome = {
+  greeting: 'Hey',
+  title: 'React'
+}
+function getTitle(title) {
+  return title;
+}
 
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
+      {/* <h1>Hello { title }</h1> */}
+      {/* {welcome.greeting} {welcome.title} */}
+      <h1>Hello {getTitle('React')}</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" />
     </div>
   );
 }
