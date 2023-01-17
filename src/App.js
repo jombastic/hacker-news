d8c29352611fef97bf9a1b5639c69dca9e3d32d3
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-17 11:16:05 +0100

    destructure item prop in the Item component

--- src/App.js
+++ src/App.js
@@ -52,20 +52,28 @@ const List = ({ list }) => {
   );
 }
 
-const Item = ({ item }) => {
+const Item = ({
+  item: {
+    title,
+    url,
+    author,
+    num_comments,
+    points
+  }
+}) => {
   return (
     <li>
       <span>
-        <a href={item.url}>{item.title}</a>
+        <a href={url}>{title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
+      <span>{author}</span>
+      <span>{num_comments}</span>
+      <span>{points}</span>
     </li>
   );
 }
 
-const Search = ({ search, onSearch }) => {  
+const Search = ({ search, onSearch }) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
