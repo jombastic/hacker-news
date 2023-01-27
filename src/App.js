3f92bdd41ed96dfbd9e20cf928d80de848155d68
Slavcho Mitrov <slavcho.mitrov@moxelle.com>
2023-01-27 16:32:01 +0100

    prevent calling of setSearchTerm when rendering the component for the first time by initializing a made up state (isMounted) in useSemiPersistentState to false

--- src/App.js
+++ src/App.js
@@ -89,10 +89,17 @@ const StyledInput = styled.input`
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 const useSemiPersistentState = (key, initialState) => {
+  const isMounted = React.useRef(false);
+
   const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
 
   React.useEffect(() => {
-    localStorage.setItem(key, value);
+    if (!isMounted.current) {
+      isMounted.current = true;
+    } else {
+      console.log('A');
+      localStorage.setItem(key, value);
+    }
   }, [key, value]);
 
   return [value, setValue];
